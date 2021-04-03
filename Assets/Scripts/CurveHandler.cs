220def967a31286d0a18a50ee7b7ed729b8567bc
Marcus <noller02@hotmail.com>
2021-04-03 20:13:02 +0200

    smooth edges on the lake, able to create shrinkable deadzones/ holes in the ice

--- Assets/Scripts/CurveHandler.cs
+++ Assets/Scripts/CurveHandler.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurveHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+    public class CurveHandler
     {
-        
-    }
+        private List<CatmullRomCurveSegment> curves;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public List<CatmullRomCurveSegment> GetCurves()
+        {
+            return curves;
+        }
+
+        public CurveHandler(Vector2[] points)
+        {
+            curves = new List<CatmullRomCurveSegment>();
+
+            // build the curve segments
+            for (int i = 0; i < points.Length; i++)
+            {
+                var curveSeg = new CatmullRomCurveSegment(
+                    points[i], 
+                    points[(i + 1) % points.Length],
+                    points[(i + 2) % points.Length],
+                    points[(i + 3) % points.Length]
+                    );
+                curves.Add(curveSeg);
+            }
+        }
+
+        public Vector2[] EvalNewPoints(int segments)
+        {
+            var list = new List<Vector2>();
+
+            float steps = 1.0f / segments;
+            foreach (var curve in curves)
+            {
+                for (int i = 1; i <= segments; i++)
+                {
+                    var u = steps * (float)i;
+                    list.Add(curve.Evaluate(u));
+                }
+            }
+            return list.ToArray();
+        }
     }
-}
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -20,6 +20,8 @@ public class MeshGenerator : MonoBehaviour
             new Vector2(150,0),
         };
 
+    public int segments = 20;
+
     public Material mat;
 
     public bool shrink = false;
@@ -28,23 +30,22 @@ public class MeshGenerator : MonoBehaviour
 
     private MeshRenderer meshRenderer;
     private Mesh msh;
-    private List<Vector2> vertices2D = new List<Vector2>();
 
     void Start()
     {
         //find additional points between 
 
+        CurveHandler curveHandler = new CurveHandler(ControlPoints);
 
-
-
+        Vector2[] vertices2D = curveHandler.EvalNewPoints(segments);
 
 
         // Use the triangulator to get indices for creating triangles
-        Triangulator tr = new Triangulator(vertices2D.ToArray());
+        Triangulator tr = new Triangulator(vertices2D);
         int[] indices = tr.Triangulate();
 
         // Create the Vector3 vertices
-        Vector3[] vertices = new Vector3[vertices2D.Count];
+        Vector3[] vertices = new Vector3[vertices2D.Length];
         for (int i = 0; i < vertices.Length; i++)
         {
             vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, 0);
